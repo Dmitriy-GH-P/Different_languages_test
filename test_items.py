e2b5a7de2f86fbcdd5d71b9dcb9077190a3e9918
Dima <mandarin666.dr@gmail.com>
2025-05-17 18:59:42 +0300

    тест по поиску кнопки (добавлены sleep и assert, селектор сделал более "уникальным")

--- test_items.py
+++ test_items.py
@@ -1,9 +1,13 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
+import time
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_for_cart_button(browser):
     browser.get(link)
-    add_button = WebDriverWait(browser, 10).until(EC.visibility_of_element_located((By.CSS_SELECTOR, "button.btn-lg")))
+    time.sleep(30)
+
+    add_button = WebDriverWait(browser, 10).until(EC.visibility_of_element_located((By.CSS_SELECTOR, "button.btn-lg.btn-primary.btn-add-to-basket")))
+    assert add_button is not None, "Кнопка не найдена или не видна"
